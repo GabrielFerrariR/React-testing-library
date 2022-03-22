5b8ecd3d31747673c9505f419fa607ee63735319
Gabriel Ferrari Ribeiro <gabrielferrari601@gmail.com>
2022-03-22 18:42:47 -0300

    Adiciona o requisto 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Testa o componente NotFound', () => {
+  test('se a página contém um heading h2 com o texto'
+  + 'Page requested not found 😭', () => {
+    render(<NotFound />);
+    const heading = screen
+      .getByRole('heading', { level: 2, name: /Page requested not found Crying emoji/i });
+    expect(heading).toBeInTheDocument();
+  });
+  test('se a página mostra uma imagem específica ', () => {
+    render(<NotFound />);
+    const altTxt = 'Pikachu crying because the page requested was not found';
+    const image = screen.getByAltText(altTxt);
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
